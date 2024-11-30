7eb9d04f63571c10efe53705fd4734a068f0e760
KoroliKatus <alereyesjuarez1@gmail.com>
2024-11-29 22:04:15 -0600

    intento de chat

--- views/chat.view.php
+++ views/chat.view.php
@@ -1,27 +1,33 @@
-<?php 
+<?php
 session_start();
 
 // Comprobar si el usuario está autenticado
-if (isset($_SESSION['user'])) {
-    // Obtener datos del perfil de usuario
+
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['user'])) {
+    header("Location: views/login.php");
+    exit();
     $user = $_SESSION['user'];
+
 }
-?> 
-
-<?php require 'partials/headerApp.php' ?>
-<?php require 'partials/sidebar.php' ?>
-
+?>
+<?php require 'partials/headerApp.php'; ?>
+<?php require 'partials/sidebar.php'; ?>
+ <html>
+<head>
+    <title>Chat</title>
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css">
+</head>
 <body>
-
     <!-- start: Chat -->
     <section class="chat-section">
         <div class="chat-container">
             <!-- start: Sidebar -->
-
             <!-- end: Sidebar -->
+
             <!-- start: Content -->
             <div class="chat-content">
-                <!-- start: Content side -->
                 <div class="content-sidebar">
                     <div class="content-sidebar-title">Chats</div>
                     <form action="" class="content-sidebar-form">
@@ -30,163 +36,67 @@ if (isset($_SESSION['user'])) {
                     </form>
                     <div class="content-messages">
                         <ul class="content-messages-list">
-                            <li class="content-message-title"><span>Recently</span></li>
-                            <li>
-                                <a href="#" data-conversation="#conversation-1">
-                                    <img class="content-message-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                    <span class="content-message-info">
-                                        <span class="content-message-name">Someone</span>
-                                        <span class="content-message-text">Lorem ipsum dolor sit amet consectetur.</span>
-                                    </span>
-                                    <span class="content-message-more">
-                                        <span class="content-message-unread">5</span>
-                                        <span class="content-message-time">12:30</span>
-                                    </span>
-                                </a>
-                            </li>
-                          
-                            <li>
-                                <a href="#">
-                                    <img class="content-message-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                    <span class="content-message-info">
-                                        <span class="content-message-name">Someone</span>
-                                        <span class="content-message-text">Lorem ipsum dolor sit amet consectetur.</span>
-                                    </span>
-                                    <span class="content-message-more">
-                                        <span class="content-message-time">12:30</span>
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul class="content-messages-list">
-                            <li class="content-message-title"><span>Recently</span></li>
-                            <li>
-                                <a href="#">
-                                    <img class="content-message-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                    <span class="content-message-info">
-                                        <span class="content-message-name">Someone</span>
-                                        <span class="content-message-text">Lorem ipsum dolor sit amet consectetur.</span>
-                                    </span>
-                                    <span class="content-message-more">
-                                        <span class="content-message-unread">5</span>
-                                        <span class="content-message-time">12:30</span>
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="content-message-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                    <span class="content-message-info">
-                                        <span class="content-message-name">Someone</span>
-                                        <span class="content-message-text">Lorem ipsum dolor sit amet consectetur.</span>
-                                    </span>
-                                    <span class="content-message-more">
-                                        <span class="content-message-time">12:30</span>
-                                    </span>
-                                </a>
-                            </li>
-                            
+                            <?php if ($chats): ?>
+                                <?php foreach ($chats as $chat): ?>
+                                    <li>
+                                        <a href="#" data-conversation="#conversation-<?= $chat['id'] ?>">
+                                            <img class="content-message-image" src="https://via.placeholder.com/50" alt="">
+                                            <span class="content-message-info">
+                                                <span class="content-message-name"><?= htmlspecialchars($chat['nombre_usuario']) ?></span>
+                                                <span class="content-message-text">Último mensaje...</span>
+                                            </span>
+                                            <span class="content-message-more">
+                                                <span class="content-message-unread">5</span>
+                                                <span class="content-message-time">12:30</span>
+                                            </span>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <li>No tienes chats activos.</li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
                 <!-- end: Content side -->
+
                 <!-- start: Conversation -->
                 <div class="conversation conversation-default active">
                     <i class="ri-chat-3-line"></i>
                     <p>Select chat and view conversation!</p>
                 </div>
-                <div class="conversation" id="conversation-1">
-                    <div class="conversation-top">
-                        <button type="button" class="conversation-back"><i class="ri-arrow-left-line"></i></button>
-                        <div class="conversation-user">
-                            <img class="conversation-user-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                            <div>
-                                <div class="conversation-user-name">Someone</div>
-                                <div class="conversation-user-status online">online</div>
-                            </div>
-                        </div>
-                        <div class="conversation-buttons">
-                            <!--
-                            <button type="button"><i class="ri-phone-fill"></i></button>-->
-                            <button type="button" id="webcamButton"><i class="ri-vidicon-line"></i></button>
-                            <button type="button"><i class="ri-information-line"></i></button>
-                        </div>
-                    </div>
-                    <div class="conversation-main">
-                        <ul class="conversation-wrapper">
-                            <div class="coversation-divider"><span>Today</span></div>
-                            <template id="conversation-template">
-
-                            <li class="conversation-item">
-                                <div class="conversation-item-side">
-                                    <img class="conversation-item-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                </div>
-                                <div class="conversation-item-content">
-                                    <div class="conversation-item-wrapper">
-                                        <div class="conversation-item-box">
-                                            <div class="conversation-item-text">
-                                                <p></p>
-                                                <div class="conversation-item-time"></div>
-                                            </div>
-                                            <div class="conversation-item-dropdown">
-                                                <button type="button" class="conversation-item-dropdown-toggle"><i class="ri-more-2-line"></i></button>
-                                                <ul class="conversation-item-dropdown-list">
-                                                    <li><a href="#"><i class="ri-share-forward-line"></i> Forward</a></li>
-                                                    <li><a href="#"><i class="ri-delete-bin-line"></i> Delete</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            </template>
-                            <template id="conversation me-template">
-                            <li class="conversation-item me">
-                                <div class="conversation-item-side">
-                                    <img class="conversation-item-image" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
-                                </div>
-                                <div class="conversation-item-content">
-                                    <div class="conversation-item-wrapper">
-                                        <div class="conversation-item-box">
-                                            <div class="conversation-item-text">
-                                                <p></p>
-                                                <div class="conversation-item-time"></div>
-                                            </div>
-                                            <div class="conversation-item-dropdown">
-                                                <button type="button" class="conversation-item-dropdown-toggle"><i class="ri-more-2-line"></i></button>
-                                                <ul class="conversation-item-dropdown-list">
-                                                    <li><a href="#"><i class="ri-share-forward-line"></i> Forward</a></li>
-                                                    <li><a href="#"><i class="ri-delete-bin-line"></i> Delete</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            </template>
-                        </ul>
-                    </div>
-                    <div class="conversation-form">
-                        <!--
-                        <button type="button" class="conversation-form-button"><i class="ri-emotion-line"></i></button>-->
-                        <button type="button" class="conversation-form-button"><i class="ri-attachment-2"></i></button>
-                        <div class="conversation-form-group">
-                            <textarea class="conversation-form-input" rows="1" placeholder="Type here..."></textarea>
-                            <!--
-                            <button type="button" class="conversation-form-record"><i class="ri-mic-line"></i></button>-->
-                        </div>
-                        
-                        <div class="checkbox-wrapper-9">Encriptar
-                            <input class="tgl tgl-flat" id="cb4-9" type="checkbox"/>
-                            <label class="tgl-btn" for="cb4-9"></label>
-                        </div>
-
-                        <button type="button" class="conversation-form-button conversation-form-submit"><i class="ri-send-plane-2-line"></i></button>
-                    </div>
-                </div>
+                <?php foreach ($chats as $chat): ?>
+                    <div class="conversation" id="conversation-<?= $chat['id'] ?>">
+    <div class="conversation-top">
+        <button type="button" class="conversation-back"><i class="ri-arrow-left-line"></i></button>
+        <div class="conversation-user">
+            <img class="conversation-user-image" src="https://via.placeholder.com/50" alt="">
+            <div>
+                <div class="conversation-user-name"><?= htmlspecialchars($chat['nombre_usuario']) ?></div>
+                <div class="conversation-user-status online">online</div>
+            </div>
+        </div>
+        <div class="conversation-buttons">
+            <button type="button" id="webcamButton"><i class="ri-vidicon-line"></i></button>
+            <button type="button"><i class="ri-information-line"></i></button>
+        </div>
+    </div>
+    <div class="conversation-main">
+        <ul class="conversation-wrapper">
+            <?php
+            // Aquí agregas los mensajes de la conversación
+            $messages = getMessagesForChat($chat['id']); // Esta función debería obtener los mensajes de la base de datos
+            foreach ($messages as $message) :
+            ?>
+                <li class="message">
+                    <span class="message-user"><?= htmlspecialchars($message['usuario_nombre']) ?>:</span>
+                    <span class="message-text"><?= htmlspecialchars($message['mensaje']) ?></span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
+<?php endforeach; ?>
                 <!-- end: Conversation -->
             </div>
             <!-- end: Content -->
